e461b737315c150f1dbf1abaf21cd05fcb087465
torsten <torsten.manhardt@cpro-iot.com>
2020-10-10 20:15:59 +0200

    init

--- webapp/test/unit/controller/App.js
+++ webapp/test/unit/controller/App.js
@@ -1,7 +1,7 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"sap/ui/demo/basicTemplate/controller/Home.controller"
+	"cmjs/controller/Home.controller"
 ], function(oController) {
 	"use strict";
 
--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -5,7 +5,7 @@ sap.ui.getCore().attachInit(function () {
 	"use strict";
 
 	sap.ui.require([
-		"sap/ui/demo/basicTemplate/test/unit/AllTests"
+		"cmjs/test/unit/AllTests"
 	], function () {
 		QUnit.start();
 	});
