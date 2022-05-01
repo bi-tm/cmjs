ec10f9d54f0eea0544d90a33de5e8ff0219fcb07
Torsten <torsten.manhardt@gmx.de>
2022-05-01 17:56:57 +0200

    404 not found redirects to root

--- cmjs/dataloader.js
+++ cmjs/dataloader.js
@@ -1,47 +1,49 @@
-var database = require('./database');
+var database = require("./database");
 var menu = require("./menu");
 var breadcrumbs = require("./breadcrumbs");
 
-module.exports = async function(request, response, next) {
+module.exports = async function (request, response, next) {
+  // get page
+  var content = await database.getPage(request.params.id);
+  if (!content) {
+    content = await database.findPages({
+      legacyUrl: request.params.id,
+    })[0];
+  }
+  if (!content) {
+    response.redirect("/");
+  } else {
+    // copy page data to repsonse.locals
+    Object.assign(response.locals, content);
 
-    // get page
-    var content = await database.getPage(request.params.id);
-    if (!content) {
-        response.locals = await database.findPages({ legacyUrl: request.params.id })[0];
+    // read menu and breadcrumbs parallel
+    var dbRequests = [
+      menu.get(!response.locals.cache),
+      breadcrumbs.get(response.locals._id),
+    ];
+    if (response.locals.parentId) {
+      // allso read parent pager
+      dbRequests.push(database.getPage(response.locals.parentId));
     }
-    if (content) {
-        // copy page data to repsonse.locals
-        Object.assign(response.locals, content);
+    const data = await Promise.all(dbRequests).catch(function (err) {
+      console.error(err);
+    });
+    response.locals.menu = data[0];
+    response.locals.breadcrumbs = data[1];
+    response.locals.parent = data[2];
 
-        // read menu and breadcrumbs parallel
-        var dbRequests = [
-            menu.get(!response.locals.cache),
-            breadcrumbs.get(response.locals._id)
-        ];
-        if (response.locals.parentId) {
-            // allso read parent pager
-            dbRequests.push(database.getPage(response.locals.parentId));
-        }
-        const data = await Promise.all(dbRequests)
-            .catch(function(err) {
-                console.error(err);
-            });
-        response.locals.menu = data[0];
-        response.locals.breadcrumbs = data[1];
-        response.locals.parent = data[2];
-
-        // read grandparent
-        if (response.locals.parent && response.locals.parent.parentId) {
-            response.locals.grandparent= await database.getPage(response.locals.parent.parentId);
-        }
-
-        // mark current entry in menu as active
-        response.locals.menu.forEach(menuItem => {
-                menuItem.active = (menuItem._id === response.locals._id);
-            })
-            // poceed
-        next();
-    } else {
-        response.status(500).end(`page ${request.params.id} not found`);
+    // read grandparent
+    if (response.locals.parent && response.locals.parent.parentId) {
+      response.locals.grandparent = await database.getPage(
+        response.locals.parent.parentId
+      );
     }
-}
+
+    // mark current entry in menu as active
+    response.locals.menu.forEach((menuItem) => {
+      menuItem.active = menuItem._id === response.locals._id;
+    });
+    // poceed
+    next();
+  }
+};
